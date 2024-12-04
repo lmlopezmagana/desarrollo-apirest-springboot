276d266e884394e26dacac2c79d3e3cf66bfabfc
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-12-04 12:45:18 +0100

    3.1 Mi primer endpoint

---
+++ rest/src/main/java/com/example/rest/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.example.rest.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/producto")
+    public Producto obtenerProducto() {
+        return new Producto(1L, "Nombre del producto");
+    }
+
+    @PostMapping("/producto")
+    public Producto nuevoProducto(@RequestBody Producto producto) {
+        return producto;
+    }
+
+    record Producto(Long id, String nombre) {
+
+    }
+
+}
+
