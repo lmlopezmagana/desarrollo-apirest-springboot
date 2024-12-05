94d7dda798c3c19f157a4865eb3018efc00873f5
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-12-05 11:07:05 +0100

    3.3 Métodos de un controlador

--- rest/src/main/java/com/example/rest/controller/ProductController.java
+++ rest/src/main/java/com/example/rest/controller/ProductController.java
@@ -4,12 +4,18 @@ package com.example.rest.controller;
 import com.example.rest.model.Product;
 import com.example.rest.model.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+@Log
 @RestController
 @RequestMapping("/product/")
 @RequiredArgsConstructor
@@ -17,10 +23,30 @@ public class ProductController {
 
     private final ProductRepository productRepository;
 
-    @GetMapping
-    public ResponseEntity<List<Product>> getAll() {
+    /*@GetMapping
+    public ResponseEntity<List<Product>> getAll(
+            @RequestParam(required = false, value = "maxPrice", defaultValue = "-1") double max,
+            @RequestParam(required = false, value = "sort", defaultValue = "no") String sortDirection
+    ) {
 
-        List<Product> result = productRepository.getAll();
+        List<Product> result = productRepository.query(max, sortDirection);
+
+        if (result.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(result);
+
+    }*/
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllv2(
+            @RequestParam Map<String, String> params
+            ) {
+
+        double max = Double.valueOf(params.getOrDefault("maxPrice", "-1"));
+        String sortDirection = params.getOrDefault("sort", "no");
+
+        List<Product> result = productRepository.query(max, sortDirection);
 
         if (result.isEmpty())
             return ResponseEntity.notFound().build();
@@ -29,10 +55,36 @@ public class ProductController {
 
     }
 
+
+
+    @GetMapping("/{id:[0-9]+}")
+    public ResponseEntity<Product> getById(@PathVariable Long id) {
+        return ResponseEntity.of(
+                productRepository.get(id)
+        );
+    }
+
+
+
     @PostMapping
     public ResponseEntity<Product> create(@RequestBody Product product) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(productRepository.add(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> edit(
+            @RequestBody Product product,
+            @PathVariable("id") Long productId) {
+
+        return ResponseEntity.of(productRepository.edit(productId, product));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        productRepository.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
