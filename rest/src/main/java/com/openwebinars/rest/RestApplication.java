f0023ac9816d1c2d21c571b01b7b6863f35efc41
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2025-01-05 13:24:49 +0100

    6.2 Documentación de un endpoint.

--- rest/src/main/java/com/openwebinars/rest/RestApplication.java
+++ rest/src/main/java/com/openwebinars/rest/RestApplication.java
@@ -1,9 +1,21 @@
 package com.openwebinars.rest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(description = "Una API REST sobre productos",
+		version = "1.0",
+		contact = @Contact(email = "luismi@openwebinars.net", name = "Luismi"),
+		license = @License(name = "CC BY"),
+		title = "API sobre productos"
+)
+)
 public class RestApplication {
 
 	public static void main(String[] args) {
--- rest/src/main/java/com/openwebinars/rest/error/GlobalErrorController.java
+++ rest/src/main/java/com/openwebinars/rest/error/GlobalErrorController.java
@@ -3,6 +3,7 @@ package com.openwebinars.rest.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -12,6 +13,7 @@ import java.net.URI;
 public class GlobalErrorController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ProductNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ProblemDetail handleProductNotFound(ProductNotFoundException ex) {
         ProblemDetail result = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
         result.setTitle("Producto no encontrado");
