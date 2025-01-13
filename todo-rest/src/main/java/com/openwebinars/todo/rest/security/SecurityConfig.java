805ad92d9140a0e5ee8f28245c452ed4f72580d4
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2025-01-13 15:30:28 +0100

    8.5.1 Gestión de errores

--- todo-rest/src/main/java/com/openwebinars/todo/rest/security/SecurityConfig.java
+++ todo-rest/src/main/java/com/openwebinars/todo/rest/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.openwebinars.todo.rest.security;
 
+import com.openwebinars.todo.rest.error.CustomAccessDeniedHandler;
+import com.openwebinars.todo.rest.error.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,12 +11,19 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,6 +31,10 @@ public class SecurityConfig {
         http
                 .cors(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
+                .exceptionHandling(excep -> {
+                    excep.accessDeniedHandler(accessDeniedHandler);
+                    excep.authenticationEntryPoint(authenticationEntryPoint);
+                })
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
                         .anyRequest().authenticated()
